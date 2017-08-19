862502fea2154bbaa3af8b599070bd15751ab511
Joel Romero <joelrg1288@gmail.com>
2017-08-18 23:33:52 -0500

    Se acomodan funciones y se completa la parte del cifrado del mensaje obtenido de un archivo de texto.

--- Prácticas/Práctica0/funciones.c
+++ Prácticas/Práctica0/funciones.c
@@ -4,10 +4,12 @@
 #define TAM_ALFABETO 26
 #include "funciones.h"
 
+//FUNCIONES DE CIFRADO AFFINE
 void cifrar (int alfa, int beta)
 {
 	int i, valor, * valores;
 	char * mensaje, * mensajeCifrado;
+	validarNumeros (alfa, beta);
 	mensaje = leerMensaje ();										//Recibimos el mensaje a cifrar y lo guardamos en un arreglo
 	printf("\nMensaje: %s\n", mensaje);								//Imprimimos el arreglo para saber que llego correctamente
 	valores = (int *) malloc (sizeof (int) * strlen (mensaje));
@@ -53,6 +55,37 @@ char * leerMensaje ()
 	return msj;														//Regresamos el archivo en un arreglo de caracteres
 }
 
+char * obtenerMsjCifrado (int * valores, char * mensaje)
+{
+	int i;
+	char c, * mensajeCifrado = (char *) malloc (sizeof (char));		//Para guardar el mensaje cifrado
+	for (i = 0; i < strlen (mensaje); i ++)
+	{
+		c = 'A' + valores [i];										//A la letra 'A' le sumamos el valor de la letra nueva
+		mensajeCifrado [i] = c;
+	}
+	mensajeCifrado [i] = '\0';										//Ponemos un final al arreglo para evitar basura
+	return mensajeCifrado;											//Regresamos el arreglo con el mensaje cifrado
+}
+
+void escribirTextoCifrado (char * mensajeCifrado)
+{
+	FILE * textoCifrado;
+	textoCifrado = fopen ("c.txt", "w");							//Creamos/Sobreescribimos el archivo 'c.txt' para escritura
+	if (textoCifrado == NULL)
+		printf("Error al crear el archivo.\n");
+	fprintf(textoCifrado, "%s", mensajeCifrado);					//Escribimos la cadena correspondiente al mensaje cifrado
+	printf("Archivo cifrado correctamente.\n");
+	fclose (textoCifrado);											//Cerramos el archivo
+}
+
+//FUNCIONES DE DESCIFRADO AFFINE
+void descifrar (int alfa, int beta)
+{
+	//
+}
+
+//FUNCIONES COMPARTIDAS DE CIFRADO/DESCIFRADO AFFINE
 int obtenerValorLetra (char letra)
 {
 	int i;
@@ -68,31 +101,40 @@ int obtenerValorLetra (char letra)
 	return i;														//Regresamos el valor de la letra siendo la a = 0, b = 1, ... z = 25
 }
 
-char * obtenerMsjCifrado (int * valores, char * mensaje)
+void validarNumeros (int alfa, int beta)
 {
-	int i;
-	char c, * mensajeCifrado = (char *) malloc (sizeof (char));		//Para guardar el mensaje cifrado
-	for (i = 0; i < strlen (mensaje); i ++)
+	if (alfa == 1)
 	{
-		c = 'A' + valores [i];										//A la letra 'A' le sumamos el valor de la letra nueva
-		mensajeCifrado [i] = c;
+		if (beta == TAM_ALFABETO)
+		{
+			printf("Error, el texto no se va a cifrar por los valores de alfa = %d y beta = %d.\n", alfa, beta);
+			exit (0);
+		}
+	}else if (alfa <= 0)
+	{
+		printf("Error, el valor de alfa debe estar dentro de 1 y el tama%co del alfabeto.\n", 164);
+		exit (0);
 	}
-	mensajeCifrado [i] = '\0';
-	return mensajeCifrado;
+	if (beta <= 0 || beta > TAM_ALFABETO)
+	{
+		printf("Error, el valor de beta debe estar dentro de 1 y el tama%co del alfabeto.\n", 164);
+		exit (0);
+	}
+	if (mcd (alfa, TAM_ALFABETO) != 1)
+	{
+		printf("Error, el texto no se va a cifrar correctamente debido al valor de alfa.\n");
+		exit (0);
+	}
 }
 
-void escribirTextoCifrado (char * mensajeCifrado)
+int mcd (int alfa, int alfabeto)
 {
-	FILE * textoCifrado;
-	textoCifrado = fopen ("c.txt", "w");
-	if (textoCifrado == NULL)
-		printf("Error al crear el archivo.\n");
-	fprintf(textoCifrado, "%s", mensajeCifrado);
-	printf("Archivo cifrado correctamente.\n");
-	fclose (textoCifrado);
-}
-
-void descifrar (int alfa, int beta)
-{
-	//
+	int temp;
+	while (alfa > 0)
+	{
+         temp = alfa;
+         alfa = alfabeto % alfa;
+         alfabeto = temp;
+     }
+     return alfabeto;
 }
--- Prácticas/Práctica0/funciones.h
+++ Prácticas/Práctica0/funciones.h
@@ -1,9 +1,13 @@
 //Funciones de cifrado
 void cifrar (int alfa, int beta);
 char * leerMensaje ();
-int obtenerValorLetra (char letra);
 char * obtenerMsjCifrado (int * valores, char * mensaje);
 void escribirTextoCifrado (char * mensajeCifrado);
 
 //Funciones de descifrado
 void descifrar (int alfa, int beta);
+
+//Funciones compartidas
+int obtenerValorLetra (char letra);
+void validarNumeros (int alfa, int beta);
+int mcd (int alfa, int alfabeto);
