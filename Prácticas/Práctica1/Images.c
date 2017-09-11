8bfaf59484fe764ed760d5202cdb9c127d04d749
Joel Romero <joelrg1288@gmail.com>
2017-09-11 02:39:12 -0500

    Se corrige nombre a cifrado de imagenes ya que se agregaran otros cifradores además de Hill.

---
+++ Prácticas/Práctica1/Images.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Functions.c"
+
+int main (int argc, char* argv[])
+{	
+	FILE * original, * encrypted;
+	bmp image;
+	int i, option;
+	char * encryptedImage = (char *) malloc (sizeof (char));
+	char * originalImage = (char *) malloc (sizeof (char));
+	system ("cls");
+	if (argc < 3)
+		printf("Error, missing arguments.\nExample: %s Image.bmp EncryptedImage.bmp\n\n", argv [0]);
+	else
+	{
+		originalImage = (char *) argv [1];
+		encryptedImage = (char *) argv [2];
+	}
+	printf("%cDo you want to encryp or decrypt?\n\n1.Encrypt\t2.Decrypt\n\n", 168);
+	scanf ("%d", &option);
+
+	//We open each file in binary mode
+	original = open_file (originalImage, encryptedImage, 1);
+	encrypted = open_file (originalImage, encryptedImage, 2);
+
+	//We read and write the head of the file
+	read_head (original, encrypted, &image);
+	if (option == 1)
+		hill (original, encrypted, &image, 'd');
+	else if (option == 2)
+		hill (original, encrypted, &image, 'e');
+	exit (0);
+}
