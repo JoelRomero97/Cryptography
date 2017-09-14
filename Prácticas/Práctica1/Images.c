468c5d1601d51c3593c519ee15adbaf72dd6ecda
Joel Romero <joelrg1288@gmail.com>
2017-09-13 20:26:48 -0500

    Se comienza a modificar funciones para utilizar todos los modos de operación.

--- Prácticas/Práctica1/Images.c
+++ Prácticas/Práctica1/Images.c
@@ -27,8 +27,10 @@ int main (int argc, char* argv[])
 	//We read and write the head of the file
 	read_head (original, encrypted, &image);
 	if (option == 1)
-		hill (original, encrypted, &image, 'e');
+		operation_mode (original, encrypted, &image, 'e')
+		//hill (original, encrypted, &image, 'e');
 	else if (option == 2)
-		hill (original, encrypted, &image, 'd');
+		operation_mode (original, encrypted, &image, 'd')
+		//hill (original, encrypted, &image, 'd');
 	exit (0);
 }
