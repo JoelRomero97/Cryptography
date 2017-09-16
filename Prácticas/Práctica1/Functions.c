faa8ed56ec16810383934f61ed4e65ccf1e6ce6f
Joel Romero <joelrg1288@gmail.com>
2017-09-15 21:27:57 -0500

    Se regresa codigo a original con solo funcion Hill.

--- Prácticas/Práctica1/Functions.c
+++ Prácticas/Práctica1/Functions.c
@@ -3,10 +3,9 @@
 #include <string.h>
 #include "Functions.h"
 
-int i;
 
-//It opens both files, and depending on the flag "type", it returns each pointer to file
-FILE * open_file (char * original, char * encrypted, int type)
+
+FILE * open_file (char * original, char * encrypted, int tipo)
 {
 	FILE * pt1, * pt2;
 	//We open the file in binary mode to read
@@ -24,7 +23,7 @@ FILE * open_file (char * original, char * encrypted, int type)
 		printf("Error while creating file: '%s'.\n", encrypted);
 		exit(1);
 	}
-	if (type == 1)
+	if (tipo == 1)
 	{
 		printf("File '%s' opened correctly.\n", original);
 		return pt1;
@@ -36,7 +35,6 @@ FILE * open_file (char * original, char * encrypted, int type)
 	}
 }
 
-//A funtion that copies all the head of the original file into the new one
 void read_head (FILE * original, FILE * encrypted, bmp * image)
 {
 	//Type (must be 'BM')
@@ -127,72 +125,26 @@ void read_head (FILE * original, FILE * encrypted, bmp * image)
 	}
 }
 
-//A simple menu to know the operation mode that user wants to use
-int selected_mode ()
+void hill (FILE * original, FILE * encrypted, bmp * image, char option)
 {
-	int selected_option;
-	printf("\n\n%cWhich mode of operation do you want to use?\n\n", 168);
-	printf("1. Electronic Codebook (ECB)\n");
-	printf("2. Cipher Block Chaining (CBC)\n");
-	printf("3. Cipher Feedback (CFB)\n");
-	printf("4. Output Feedback (OFB)\n");
-	printf("5. Counter (CTR)\n\n");
-	scanf ("%d", &selected_option);
-	return selected_option;
-}
-
-//It calls each operation mode depending on the selected one that we obtain from function "selected_mode"
-void operation_mode (FILE * original, FILE * encrypted, bmp * image, char option)
-{
-	int selected_option;
-	selected_option = selected_mode ();
-	switch (selected_option)
-	{
-		case 1:
-			ECB (original, encrypted, image, option);
-			break;
-		case 2:
-			//CBC (original, encrypted, image, option);
-			break;
-		case 3:
-			//CFB (original, encrypted, image, option);
-			break;
-		case 4:
-			//OFB (original, encrypted, image, option);
-			break;
-		case 5:
-			//CTR (original, encrypted, image, option);
-			break;
-		default:
-			operation_mode (original, encrypted, image, option);
-	}
-}
-
-void ECB (FILE * original, FILE * encrypted, bmp * image, char option)
-{
-	unsigned char BGR [3], pixel [3];
-	//unsigned char * pixel = (char *) malloc (sizeof (char) * 3);
+	int i, j;
+	unsigned char red, blue, green, pixel [3];
 	for (i = 0; i < (image -> image_size); i ++)
 	{
-		fread (&BGR, sizeof (unsigned char), 3, original);
-		hill (BGR, option, &pixel);
-		//printf("%d\t%d\t%d\n\n", pixel [0], pixel [1], pixel [2]);
-		//getchar ();
-		fwrite (&pixel, sizeof (unsigned char), 3, encrypted);
+		fread (&blue, sizeof (char), 1, original);
+		fread (&green, sizeof (char), 1, original);
+		fread (&red, sizeof (char), 1, original);
+		for (j = 0; j < 3; j ++)
+		{
+			if (option == 'd')				//D from decryption
+				pixel [j] = ((blue * key.Dk [0][j]) + (green * key.Dk [1][j]) + (red * key.Dk [2][j])) % 256;
+			else
+				pixel [j] = ((blue * key.Ek [0][j]) + (green * key.Ek [1][j]) + (red * key.Ek [2][j])) % 256;
+		}
+		fwrite (&pixel, sizeof (char), 3, encrypted);
+		memset (pixel, 0, 3);
 	}
-}
-
-void hill (unsigned char * BGR, char option, unsigned char * pixel)
-{
-	//unsigned char * pixel = (char *) malloc (sizeof (char) * 3);
-	//unsigned char pixel [3];
-	memset (pixel, 0, 3);
-	for (i = 0; i < 3; i ++)
-	{
-		if (option == 'd')				//D from decryption
-			pixel [i] = ((BGR [0] * key.Dk [0][i]) + (BGR [1] * key.Dk [1][i]) + (BGR [2] * key.Dk [2][i])) % 256;
-		else
-			pixel [i] = ((BGR [0] * key.Ek [0][i]) + (BGR [1] * key.Ek [1][i]) + (BGR [2] * key.Ek [2][i])) % 256;
-	}
-	return;
+	//We close each file
+	fclose (original);
+	fclose (encrypted);
 }
