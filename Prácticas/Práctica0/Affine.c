8f9edfb54b5ac6cbb8ff7a4e76b8fbd85559e0d8
Joel Romero <joelrg1288@gmail.com>
2017-10-13 23:51:06 -0500

    Se agrega encabezado de autoría al programa

--- Prácticas/Práctica0/Affine.c
+++ Prácticas/Práctica0/Affine.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include "functions.c"
 
+////////////////////////////////////////////////////////////////////////////////////////
+////									Affine.c 									////
+////																				////
+////																				////
+//// Implementación en lenguaje C de un cifrador Affine que recibe 2 parámetros, 	////
+//// Alfa (multiplicativo) y Beta (Aditivo), para hacer el correcto cifrado de un 	////
+//// mensaje, leyéndolo de un archivo 'm.txt' y escribiendo el mensaje cifrado en 	////
+//// 'c.txt'. Para descifrar, lo lee del archivo 'c.txt' y lo imprime en consola.	////
+//// Modificar archivo "m.txt".														////
+////																				////
+////																				////
+//// Autor: Romero Gamarra Joel Mauricio											////
+////////////////////////////////////////////////////////////////////////////////////////
 
 int main (int argc, char const *argv[])
 {
